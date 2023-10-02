890f2d804cbf882a75c50af0bad3b0a80c2a6d28
Haloegen <>
2023-10-02 10:24:31 +0000

    Initial set up for battle ship game

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+board = [[]]
+
+board_size = 10
+
+ship_count = 8
+
+shots_left = 50
+
+game_over = false 
+
+ships_sunk = 0
+
+position_of_ships = [[]]
+
+alphabet = "ABCDEFGHJKLMNOPQRSTUVWXYZ"
