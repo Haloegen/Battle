19b50232292f670ffddda71bb584c5091bd1f848
Haloegen <>
2023-10-02 10:39:49 +0000

    Add code for creating the board for the placing of ships

--- run.py
+++ run.py
@@ -19,10 +19,39 @@ position_of_ships = [[]]
 alphabet = "ABCDEFGHJKLMNOPQRSTUVWXYZ"
 
 
-def grid_and_ship_location:
+def grid_and_ship_location(start_row, end_row, start_col, end_col):
     """
     Will check to see if the location of the ship is valid
     Returns true of false
     """
     global board
     global position_of_ships
+
+    pass
+
+
+def place_ships_on_board(row, col, direction, length):
+    """
+    Will try and place a ship will return true or false if ship location is
+    invalid
+    """
+    global board_size
+
+    pass
+
+    return valid_grid_and_location(0, 0, 0, 0)
+
+
+def create_board():
+    """
+    Will create the board and place down ships in valid locations
+    Ships will be of different sizes
+    """
+    global board
+    global board_size
+    global ship_count
+    global position_of_ships
+
+    pass
+
+    try_placing_ships_onboard(0, 0, 0, 0)
